cbaeec6f195c19a631f20bc13990c519a94943bc
Logan Larson <loganlarson20@gmail.com>
2023-08-09 15:49:01 -0500

    Added Blazor client front end

---
+++ PixClient/Shared/Message.cs
@@ -0,0 +1,14 @@
+﻿
+using Newtonsoft.Json;
+
+namespace PixClient.Shared
+{
+    public class Message
+    {
+        public required string role { get; set; }
+        public required string content { get; set; }
+
+        [JsonIgnore]
+        public DateTime timestamp { get; set; }
+    }
+}
---
+++ PixClient/wwwroot/speechRecognition.js
@@ -0,0 +1,55 @@
+﻿var recognition;
+var isListeningForAwakeKeyword = true;
+
+var resumeKeyword = "keep going";
+var cancelKeyword = "stop";
+var eraseKeyword = "scratch";
+var sendKeyword = "go";
+
+const bot = window.speechSynthesis;
+
+function startRecognition(resultCallback) {
+    recognition = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = 'en-US'; // You can change the language here
+    recognition.continuous = true;
+
+    recognition.onresult = function (event) {
+        var result = event.results[event.results.length - 1];
+
+        if (result.isFinal) {
+
+            if (bot.speaking) {
+                bot.pause();
+            }
+
+            var transcript = result[0].transcript;
+
+            if (transcript.toLowerCase().includes(resumeKeyword)) {
+                bot.resume();
+            } else if (transcript.toLowerCase().includes(cancelKeyword)) {
+                bot.cancel();
+            } else if (transcript.toLowerCase().includes(eraseKeyword)) {
+                resultCallback.invokeMethodAsync('EraseBuffer');
+            } else if (transcript.toLowerCase().includes(sendKeyword)) {
+                resultCallback.invokeMethodAsync('SendBuffer');
+            } else {
+                resultCallback.invokeMethodAsync('AppendBuffer', transcript);
+            }
+        }
+    };
+
+    recognition.start();
+}
+
+function stopRecognition() {
+    if (recognition) {
+        recognition.stop();
+    }
+}
+
+
+function speakText(text)  {
+    var msg = new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(msg);
+}
